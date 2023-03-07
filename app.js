59b5a355be234a0bd65ab54ddab8b5d170340956
karunjohn <karunkjohn@outlook.com>
2023-03-07 11:10:34 +0530

    schema created

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 // Task1: initiate app and run server at 3000
 const express=require('express')
 const app=express()
+
+app.use(express.json())//parse json data
+app.use(express.urlencoded({extended:true}))
+
+
 app.listen(3000,()=>{
     console.log('server is running at port 3000')
 })
 
 const path=require('path');
 app.use(express.static(path.join(__dirname+'/dist/FrontEnd')));
+
 // Task2: create mongoDB connection 
 const mongoose = require('mongoose');
 main().catch(err => console.log(err));
@@ -16,6 +22,8 @@ async function main() {
   
    await mongoose.connect('mongodb+srv://karunkjohn:qwerty123@cluster0.2ucz2a9.mongodb.net/test');
 }
+//importing model
+const employeeData=require('./model/employee')
 
 
 //Task 2 : write api with error handling and appropriate api mentioned in the TODO below
@@ -27,7 +35,18 @@ async function main() {
 
 
 //TODO: get data from db  using api '/api/employeelist'
+app.get('/api/employeelist',async(req,res)=>{
+    try {
+      const employeelist= await employeeData.find({})
+      res.send(employeelist)
+        
+    }catch (error) {
+        console.log(error)
+        res.json('Error')
+        
+    }
 
+})
 
 
 
